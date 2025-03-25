ed6f495aecfddab70eb6c2bffe82fc153bd3ddf2
Dixon Mathews <118825481+CaptModTerm13@users.noreply.github.com>
2025-03-25 15:43:41 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -100,7 +100,7 @@ always@(posedge clk)begin
 end
 endmodule
 
-module tt_CaptModTerm13_UART(
+module tt_um_UART(
     input  wire [7:0] ui_in,
     output wire [7:0] uo_out,
     input  wire [7:0] uio_in,
